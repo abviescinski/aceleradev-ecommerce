6d09b1f2cca954c5345db4b746be9148d0ffe299
abviescinski <amanda.b.vieski@gmail.com>
2021-12-07 18:07:58 -0300

    implements user and auth part 1

---
+++ app/api/auth/views.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.param_functions import Depends
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from app.repositories.user_repository import UserRespository
+from app.services.auth_service import create_token
+import bcrypt
+
+router = APIRouter()
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
+
+
+@router.get('/')
+def index(token: str = Depends(oauth_scheme)):
+    return {'hello': True}
+
+
+@router.post('/login')
+def login(form_data: OAuth2PasswordRequestForm = Depends(), user_repository: UserRespository = Depends()):
+    user = user_repository.find_by_email(form_data.username)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='User not found with this username.')
+    if not bcrypt.checkpw(form_data.password.encode('utf8'), user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail='The password id invalid.')
+    return {'access_token': create_token({'id': user.id}), 'token_type': 'bearer'}
--- app/api/router.py
+++ app/api/router.py
@@ -7,6 +7,8 @@ from .product_discount.views import router as product_discount_router
 from .coupon.views import router as coupon_router
 from .address.views import router as address_router
 from .customer.views import router as customer_router
+from .auth.views import router as auth_router
+from .user.views import router as user_router
 
 router = APIRouter()
 
@@ -20,3 +22,5 @@ router.include_router(product_discount_router,
 router.include_router(coupon_router, prefix='/coupon', tags=['coupon'])
 router.include_router(address_router, prefix='/address', tags=['address'])
 router.include_router(customer_router, prefix='/customer', tags=['customer'])
+router.include_router(auth_router, prefix='/auth', tags=['auth'])
+router.include_router(user_router, prefix='/user', tags=['user'])
